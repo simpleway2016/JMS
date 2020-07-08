2799b99d3a051e22be0ff44736751d444e3c11ac
Jack <Jack@DESKTOP-9GB39PB>
2020-07-08 11:54:25 +0800

    事务id持久化

--- JMS.Gateway/IDBuilders/TransactionIdBuilder.cs
+++ JMS.Gateway/IDBuilders/TransactionIdBuilder.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading;
 
@@ -8,9 +11,40 @@ namespace JMS
     class TransactionIdBuilder
     {
         static int CurrentId = 0;
+        ILogger<TransactionIdBuilder> _logger;
+        AutoResetEvent _waitObject = new AutoResetEvent(false);
+        string _filepath;
+        public TransactionIdBuilder(IConfiguration configuration, ILogger<TransactionIdBuilder> logger)
+        {
+            _logger = logger;
+            var datafolder = configuration.GetValue<string>("DataFolder");
+            _filepath = $"{datafolder}/TransactionIdBuilder.txt";
+            if (File.Exists(_filepath))
+            {
+                CurrentId = Convert.ToInt32(File.ReadAllText(_filepath, Encoding.UTF8)) + 1000;
+            }
+            new Thread(saveToFile).Start();
+        }
+        void saveToFile()
+        {
+            while(true)
+            {
+                try
+                {
+                    _waitObject.WaitOne();
+                    File.WriteAllText(_filepath, CurrentId.ToString(), Encoding.UTF8);
+                }
+                catch (Exception ex)
+                {
+                    _logger?.LogError(ex, ex.Message);
+                }
+            }
+        }
         public string Build()
         {
-            return Interlocked.Increment(ref CurrentId).ToString();
+            var ret = Interlocked.Increment(ref CurrentId).ToString();
+            _waitObject.Set();
+            return ret;
         }
     }
 }
