5c94095c921fc2bada8d7d3c059860ae8567e204
89687 <89687@DESKTOP-Q5P2HGQ>
2020-07-26 13:05:23 +0800

    add DynamicBuilder

---
+++ JMS.ServiceProvider/DynamicBuilders/IDynamicInovker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JMS.DynamicBuilders
+{
+    public interface IDynamicInovker
+    {
+        object Invoke(MicroServiceControllerBase ctrl, object[] parameters);
+    }
+}
