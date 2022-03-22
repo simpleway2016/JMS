11631351d1b4ee5af04b05d1df7acf8d748194c2
Jack.T <896872647@qq.com>
2022-03-22 17:26:16 +0800

    包含子文件夹到zip

--- Publish/publish in windows.bat
+++ Publish/publish in windows.bat
@@ -5,9 +5,9 @@ dotnet publish JMS.Gateway.Referee\JMS.Gateway.Referee.csproj -c release -o Publ
 dotnet publish JMS.Proxy\JMS.Proxy.csproj -c release -o Publish\Proxy --self-contained false
 dotnet publish ServiceStatusViewer\ServiceStatusViewer.csproj -c release -o Publish\Tools\ServiceStatusViewer --self-contained false
 
-"C:\Program Files\WinRAR\winrar.exe" a -ep %~dp0Gateway.zip %~dp0Gateway
-"C:\Program Files\WinRAR\winrar.exe" a -ep %~dp0TokenServer.zip %~dp0TokenServer
-"C:\Program Files\WinRAR\winrar.exe" a -ep %~dp0GatewayReferee.zip %~dp0GatewayReferee
-"C:\Program Files\WinRAR\winrar.exe" a -ep %~dp0Proxy.zip %~dp0Proxy
-"C:\Program Files\WinRAR\winrar.exe" a -ep %~dp0ServiceStatusViewer.zip %~dp0Tools\ServiceStatusViewer
+"C:\Program Files\WinRAR\winrar.exe" a -ep1 %~dp0Gateway.zip %~dp0Gateway
+"C:\Program Files\WinRAR\winrar.exe" a -ep1 %~dp0TokenServer.zip %~dp0TokenServer
+"C:\Program Files\WinRAR\winrar.exe" a -ep1 %~dp0GatewayReferee.zip %~dp0GatewayReferee
+"C:\Program Files\WinRAR\winrar.exe" a -ep1 %~dp0Proxy.zip %~dp0Proxy
+"C:\Program Files\WinRAR\winrar.exe" a -ep1 %~dp0ServiceStatusViewer.zip %~dp0Tools\ServiceStatusViewer
 pause
