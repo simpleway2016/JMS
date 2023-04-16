d43f96868a5e7bb660271c6a38e94be5df08a293
Jack <Jack@DESKTOP-0GATFNU>
2023-04-16 18:59:33 +0800

    add ValueChanged event

--- JMS.Common/ConfigurationValue.cs
+++ JMS.Common/ConfigurationValue.cs
@@ -7,6 +7,7 @@ namespace JMS.Common
 {
     public class ConfigurationValue<T>
     {
+        public event EventHandler<ValueChangedArg<T>> ValueChanged;
         /// <summary>
         /// 转换后对象，对象会自动随着配置文件更新而更新
         /// </summary>
@@ -21,8 +22,26 @@ namespace JMS.Common
         {
             _disposable?.Dispose();
             IConfiguration configuration = (IConfiguration)state;
-            this.Current = configuration.Get<T>();
+            var old = this.Current;
+            this.Current = configuration.Get<T>();           
             _disposable = configuration.GetReloadToken().RegisterChangeCallback(callback, configuration);
+            try
+            {
+                this.ValueChanged?.Invoke(this, new ValueChangedArg<T>
+                {
+                    OldValue = old,
+                    NewValue = this.Current
+                });
+            }
+            catch
+            {
+            }
         }
+    }
+
+    public class ValueChangedArg<T>
+    {
+        public T OldValue { get; set; }
+        public T NewValue { get; set; }
     }
 }
