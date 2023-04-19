a2481002614d8dbb19e0418631c35546ab33f752
Jack <Jack@DESKTOP-0GATFNU>
2023-04-19 18:09:45 +0800

    更改命名冲突

--- JMS.Token.AspNetCore/Extens.cs
+++ JMS.Token.AspNetCore/Extens.cs
@@ -12,19 +12,6 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class Extens
     {
-        /// <summary>
-        /// 使用JMS.Token作为身份验证
-        /// </summary>
-        /// <param name="services"></param>
-        /// <param name="serverAddress">token服务器地址,null表示不需要服务器，本地自行验证</param>
-        /// <param name="headerName">客户端通过哪个头部传递token</param>
-        /// <param name="authCallback">验证通过后，回调的函数，callback如果返回false，表示验证失败
-        /// 三个参数分别是 token content ticket
-        /// </param>
-        public static AuthenticationBuilder AddJmsTokenAuthentication(this IServiceCollection services,  NetAddress serverAddress, string headerName = "Authorization", Func<AuthenticationParameter, bool> authCallback = null)
-        {
-            return AddJmsTokenAuthentication(services, serverAddress, new string[] { headerName }, authCallback);
-        }
 
         /// <summary>
         /// 使用JMS.Token作为身份验证
@@ -35,7 +22,7 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <param name="authCallback">验证通过后，回调的函数，callback如果返回false，表示验证失败
         /// 三个参数分别是 token content ticket
         /// </param>
-        public static AuthenticationBuilder AddJmsTokenAuthentication(this IServiceCollection services, NetAddress serverAddress, string[] headerNames, Func<AuthenticationParameter, bool> authCallback = null)
+        public static AuthenticationBuilder AddJmsTokenAspNetCore(this IServiceCollection services, NetAddress serverAddress, string[] headerNames, Func<AuthenticationParameter, bool> authCallback = null)
         {
             AuthHandler.HeaderNames = headerNames;
             AuthHandler.ServerAddress = serverAddress;
