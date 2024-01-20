1887bbf080d8666915d73eca0d2d842de15ccd91
Jack <896872647@qq.com>
2024-01-20 12:42:25 +0800

    Delete Publish directory

--- Publish/publish 独立环境.bat
+++
@@ -1,4 +0,0 @@
-cd /d "%~dp0..\"
-dotnet publish JMS.Gateway\JMS.Gateway.csproj -c release -o Publish\Linux\Gateway --self-contained true --runtime linux-x64
-dotnet publish JMS.TokenServer\JMS.TokenServer.csproj -c release -o Publish\Linux\TokenServer --self-contained true --runtime linux-x64
-pause
