4705133a72a935349c240d763b92f6fc2e1d3061
jack <896872647@qq.com>
2024-06-24 14:20:51 +0800

    Avoid spaces in network addresses

--- JMS.Common/Extensions.cs
+++ JMS.Common/Extensions.cs
@@ -3,7 +3,10 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.Buffers;
 using System.Collections.Generic;
+using System.Net.WebSockets;
 using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
 using Way.Lib;
 
 namespace JMS
@@ -61,5 +64,167 @@ namespace JMS
                 }
             }
         }
+    }
+}
+
+public static class WebSocketExtens
+{
+    public static async Task<string> ReadString(this WebSocket webSocket)
+    {
+        byte[] data = ArrayPool<byte>.Shared.Rent(4096);
+        List<byte> list = null;
+        try
+        {
+            var buffer = new ArraySegment<byte>(data);
+            int len;
+            while (true)
+            {
+                var ret = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
+                if (ret.CloseStatus != null)
+                {
+                    throw new WebSocketException(ret.CloseStatusDescription);
+                }
+                len = ret.Count;
+                if (ret.EndOfMessage)
+                {
+                    if (list != null)
+                    {
+                        list.AddRange(buffer.Slice(0, ret.Count));
+                    }
+                    break;
+                }
+                else if (len > 0)
+                {
+                    if (list == null)
+                        list = new List<byte>();
+                    list.AddRange(buffer.Slice(0, ret.Count));
+                    if (list.Count > 102400)
+                    {
+                        list.Clear();
+                        throw new Exception("websocket data is too big");
+                    }
+                }
+            }
+            if (list != null)
+                return Encoding.UTF8.GetString(list.ToArray());
+            else
+                return Encoding.UTF8.GetString(data, 0, len);
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(data);
+            list?.Clear();
+        }
+    }
+
+    public static async Task<string> ReadString(this WebSocket webSocket, CancellationToken cancellationToken)
+    {
+        byte[] data = ArrayPool<byte>.Shared.Rent(4096);
+        List<byte> list = null;
+        try
+        {
+            var buffer = new ArraySegment<byte>(data);
+            int len;
+            while (true)
+            {
+                var ret = await webSocket.ReceiveAsync(buffer, cancellationToken);
+                if (ret.CloseStatus != null)
+                {
+                    throw new WebSocketException(ret.CloseStatusDescription);
+                }
+                len = ret.Count;
+                if (ret.EndOfMessage)
+                {
+                    if (list != null)
+                    {
+                        list.AddRange(buffer.Slice(0, ret.Count));
+                    }
+                    break;
+                }
+                else if (len > 0)
+                {
+                    if (list == null)
+                        list = new List<byte>();
+                    list.AddRange(buffer.Slice(0, ret.Count));
+                    if (list.Count > 102400)
+                    {
+                        list.Clear();
+                        throw new Exception("websocket data is too big");
+                    }
+                }
+            }
+            if (list != null)
+                return Encoding.UTF8.GetString(list.ToArray());
+            else
+                return Encoding.UTF8.GetString(data, 0, len);
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(data);
+            list?.Clear();
+        }
+    }
+
+    public static async Task<byte[]> ReadBinary(this WebSocket webSocket, CancellationToken cancellationToken)
+    {
+        byte[] data = ArrayPool<byte>.Shared.Rent(4096);
+        List<byte> list = null;
+        try
+        {
+            var buffer = new ArraySegment<byte>(data);
+            int len;
+            while (true)
+            {
+                var ret = await webSocket.ReceiveAsync(buffer, cancellationToken);
+                if (ret.CloseStatus != null)
+                {
+                    throw new WebSocketException(ret.CloseStatusDescription);
+                }
+                len = ret.Count;
+                if (ret.EndOfMessage)
+                {
+                    if (list != null)
+                    {
+                        list.AddRange(buffer.Slice(0, ret.Count));
+                    }
+                    break;
+                }
+                else if (len > 0)
+                {
+                    if (list == null)
+                        list = new List<byte>();
+                    list.AddRange(buffer.Slice(0, ret.Count));
+                    if (list.Count > 102400)
+                    {
+                        list.Clear();
+                        throw new Exception("websocket data is too big");
+                    }
+                }
+            }
+            if (list != null)
+                return list.ToArray();
+            else
+                return buffer.Slice(0, len).ToArray();
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(data);
+            list?.Clear();
+        }
+    }
+
+    public static Task SendString(this WebSocket webSocket, string text)
+    {
+        var senddata = Encoding.UTF8.GetBytes(text);
+        return webSocket.SendAsync(new ArraySegment<byte>(senddata), WebSocketMessageType.Text, true, CancellationToken.None);
+    }
+    public static Task SendString(this WebSocket webSocket, string text, CancellationToken cancellationToken)
+    {
+        var senddata = Encoding.UTF8.GetBytes(text);
+        return webSocket.SendAsync(new ArraySegment<byte>(senddata), WebSocketMessageType.Text, true, cancellationToken);
+    }
+    public static Task SendBinary(this WebSocket webSocket, byte[] data, int offset, int count, CancellationToken cancellationToken)
+    {
+        return webSocket.SendAsync(new ArraySegment<byte>(data, offset, count), WebSocketMessageType.Text, true, cancellationToken);
     }
 }
