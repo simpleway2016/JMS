95d4f012939d864ec00c93caa18f4f86549b4a02
jack <896872647@qq.com>
2024-11-29 14:47:22 +0800

    read version from .csproj

--- Publish/publish 独立环境 - Gateway.bat
+++ Publish/publish 独立环境 - Gateway.bat
@@ -1,5 +1,14 @@
 cd /d "%~dp0..\"
-set version=3.5.9
+
+:: 设置 XML 文件路径和要查找的节点名称
+set "XML_FILE=JMS.Gateway\JMS.Gateway.csproj"
+set "NODE_NAME=Version"
+
+:: 使用 PowerShell 读取 XML 节点内容
+for /f "delims=" %%a in ('powershell -Command "([xml](Get-Content '%XML_FILE%')).SelectSingleNode('//%NODE_NAME%').'#text'"') do (
+    set "version=%%a"
+)
+
 dotnet publish JMS.Gateway\JMS.Gateway.csproj -c release -o Publish\Linux\Gateway --self-contained true --runtime linux-x64
 "C:\Program Files\WinRAR\winrar.exe" a -ep1 %~dp0Gateway%version%.linux.zip %~dp0Linux\Gateway
 
