1831b9bc6e0fce0502b537f6b87eb61d6bef914e
simpleman2000 <simpleman2000@noreply.gitcode.com>
2024-12-23 16:18:02 +0800

    auto read version

--- Publish/docker - Gateway - 打包.bat
+++ Publish/docker - Gateway - 打包.bat
@@ -1,6 +1,15 @@
 @chcp 65001
 cd /d "%~dp0..\"
-set version=3.5.9
+
+:: 设置 XML 文件路径和要查找的节点名称
+set "XML_FILE=JMS.Gateway\JMS.Gateway.csproj"
+set "NODE_NAME=Version"
+
+:: 使用 PowerShell 读取 XML 节点内容
+for /f "delims=" %%a in ('powershell -Command "([xml](Get-Content '%XML_FILE%')).SelectSingleNode('//%NODE_NAME%').'#text'"') do (
+    set "version=%%a"
+)
+
 dotnet publish JMS.Gateway\JMS.Gateway.csproj -c release -o Publish\Linux\GatewayDocker --self-contained false --runtime linux-x64
 cd Publish
 docker build -t jackframework/jmsgateway:%version% -f dockerfile_gateway .
--- Publish/docker - Webapi - 打包.bat
+++ Publish/docker - Webapi - 打包.bat
@@ -1,6 +1,14 @@
 @chcp 65001
 cd /d "%~dp0..\"
-set version=3.5.9
+:: 设置 XML 文件路径和要查找的节点名称
+set "XML_FILE=JMS.WebApi\JMS.WebApi.csproj"
+set "NODE_NAME=Version"
+
+:: 使用 PowerShell 读取 XML 节点内容
+for /f "delims=" %%a in ('powershell -Command "([xml](Get-Content '%XML_FILE%')).SelectSingleNode('//%NODE_NAME%').'#text'"') do (
+    set "version=%%a"
+)
+
 dotnet publish JMS.WebApi\JMS.WebApi.csproj -c release -o Publish\Linux\WebApiDocker --self-contained true --runtime linux-x64
 cd Publish
 docker build -t jackframework/jmswebapi:%version% -f dockerfile_webapi .
