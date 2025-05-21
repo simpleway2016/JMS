72f533919a7631176dd03760c4633a7d55e67bfb
RunYu2014 <896872647@qq.com>
2025-05-21 18:16:49 +0800

    更改证书有效期判断逻辑

--- JMS.HttpProxy/AutoGenerateSslCert/SslCertGenerator.cs
+++ JMS.HttpProxy/AutoGenerateSslCert/SslCertGenerator.cs
@@ -105,13 +105,6 @@ namespace JMS.HttpProxy.AutoGenerateSslCert
             generate();
         }
 
-
-        async void disposeCert(X509Certificate2 cert)
-        {
-            await Task.Delay(3000);
-            cert?.Dispose();
-        }
-
         async void generate()
         {
             var path = $"${_acmeConfig.Domain}.pfx";
@@ -173,7 +166,7 @@ namespace JMS.HttpProxy.AutoGenerateSslCert
                         OrganizationUnit = "Dev",
                     }, path, _acmeConfig.Password);
 
-                    disposeCert(cert);
+                    cert?.Dispose();
                     cert = X509CertificateLoader.LoadPkcs12FromFile(path, _acmeConfig.Password);
 
                     _logger.LogInformation($"域名：{_acmeConfig.Domain} 成功生成证书{path}，有效期到：{cert.NotAfter.ToString("yyyy-MM-dd HH:mm")}");
